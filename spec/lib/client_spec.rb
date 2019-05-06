f2027d97ba3d83937504f31ac0919ca5bc730f33
emono <ababababa0222@gmail.com>
2019-05-06 22:26:38 +0900

    fix spec

--- spec/lib/client_spec.rb
+++ spec/lib/client_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Line::Notify::Client do
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    expect(Line::Notify::Client.greet).to eq("Hello")
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require "bundler/setup"
-require "lib/client"
+require "client"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
